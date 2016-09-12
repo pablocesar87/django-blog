5e5eaf8326dce1c83940ab6731161c0e20f4b4bd
pablocesar87 <pablocesar1987@gmail.com>
2016-09-12 13:13:19 +0200

    fixed how post are shown

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
+from django.core.urlresolvers import reverse
+
 from . import views
 
-
-urlpatterns=[
+urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
